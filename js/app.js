c66fdb2952f630ee4dec6256af737b689196735a
Chun Hin D. Tang <dtang955472@gmail.com>
2021-08-25 13:26:19 -0400

    replaced nonresponding google sheets with a json file for the project

--- js/app.js
+++ js/app.js
@@ -1,26 +1,31 @@
+import projects from "../projectData.json"
+
 ///////////////////////////////////////////////////
 // Get data from Google Sheets
 ///////////////////////////////////////////////////
 
-$.ajax("https://spreadsheets.google.com/feeds/list/1gG9ADNvPO4dN1fL4r-pTxV-QYtfJ_8SZyhzLTCyNzq8/1/public/full?alt=json")
+// $.ajax("https://spreadsheets.google.com/feeds/list/1gG9ADNvPO4dN1fL4r-pTxV-QYtfJ_8SZyhzLTCyNzq8/1/public/full?alt=json")
 
-// .then for when the data arrives
-.then((data) => {
-    console.log(data)
+// // .then for when the data arrives
+// .then((data) => {
+//     console.log(data)
 
-    // Map over the data to generate a simplier data
-    const projects = data.feed.entry.map((item) => {
-        return {
-            project: item.gsx$project.$t,
-            liveurl: item.gsx$liveurl.$t,
-            giturl: item.gsx$giturl.$t,
-            image: item.gsx$image.$t,
-            description: item.gsx$description.$t,
-        }
-    })
+//     // Map over the data to generate a simplier data
+//     const projects = data.feed.entry.map((item) => {
+//         return {
+//             project: item.gsx$project.$t,
+//             liveurl: item.gsx$liveurl.$t,
+//             giturl: item.gsx$giturl.$t,
+//             image: item.gsx$image.$t,
+//             description: item.gsx$description.$t,
+//         }
+//     })
 
-    console.log(projects)
+//     console.log(projects)
 
+///////////////////////////////////////////////////
+// Get data from Google Sheets
+///////////////////////////////////////////////////
 
     /////////////////////////////////////////////////
     // jquery to render your project
